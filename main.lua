d210101a84f47c46094cdf2aa4fbca8e01e965ab
penotrat0r <idk016863@gmail.com>
2024-11-28 22:15:14 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,6 @@
-
+local PlaceId = game.PlaceId
+if PlaceId == 13815196156 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/penotrat0r/hellmet-main/refs/heads/main/lobby.lua')())
+else
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/penotrat0r/hellmet-main/refs/heads/main/script-main.lua')())
+end
